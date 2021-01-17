f8c9511cb4afe1a6a41cb5ab904cbadc47025409
c-o-o-k-i-e-s <69985466+c-o-o-k-i-e-s@users.noreply.github.com>
2021-01-17 09:39:53 -0500

    Uploaded the scripts
    The ButtonHandler script deals with what happens when clues are pressed. 
    THe DisplayImage script displays the correct background and clues associated with that background.

---
+++ ButtonHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    private DisplayImage currentDisplay;
+    //public Button Clue1;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentDisplay = GameObject.Find("displayObject").GetComponent<DisplayImage>();
+    }
+
+    public void OnCouponPress() {
+    	GameObject.Find("Clue1").gameObject.SetActive(true);
+    }
+
+    public void OnRightClickArrow() {
+    	currentDisplay.CurrentScene = currentDisplay.CurrentScene + 1;
+    	
+    	if (currentDisplay.CurrentScene == 1)
+        	GameObject.Find("Clue #1").SetActive(true);
+        else if (currentDisplay.CurrentScene != 1)
+        	GameObject.Find("Clue #1").SetActive(false);
+        showOrHideClue();
+    }
+
+    public void OnLeftClickArrow() {
+    	currentDisplay.CurrentScene = currentDisplay.CurrentScene - 1;
+    	if (currentDisplay.CurrentScene == 1)
+        	GameObject.Find("Clue #1").SetActive(true);
+        else if (currentDisplay.CurrentScene != 1)
+        	GameObject.Find("Clue #1").SetActive(false);
+    	showOrHideClue();
+    }
+
+    private void showOrHideClue() {
+		// GetComponent<SpriteRenderer>().sprite == Resources.Load<Sprite>("Sprites/Scene1");
+
+		if (currentDisplay.CurrentScene == 1)
+        	GameObject.Find("Clue #1").SetActive(true);
+        else if (currentDisplay.CurrentScene != 1)
+        	GameObject.Find("Clue #1").SetActive(false);
+    }
+
+    // Update is called once per frame
+   // void Update() {
+        
+   //  }
+}
---
+++ DisplayImage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayImage : MonoBehaviour
+{
+ 	private int currentScene;
+	private int previousScene;
+
+    // public void ChangeScene(string ClueName) {
+   	// 	Application.LoadLevel(clueName);
+    // }
+
+    // Start is called before the first frame update
+    void Start() {
+        previousScene = 0;
+        currentScene = 1;
+        GameObject.Find("Clue1").SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentScene != previousScene) {
+        	GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/Scene"+currentScene.ToString());
+        }
+        previousScene = currentScene;
+		if (GetComponent<SpriteRenderer>().sprite == Resources.Load<Sprite>("Sprites/Scene1"))
+        	GameObject.Find("Clue #1").SetActive(true);
+        else
+        	GameObject.Find("Clue #1").SetActive(false);
+    }
+
+       public int CurrentScene {
+		get {
+			return currentScene;
+		} set {
+			if (value == 4)
+				currentScene = 1;
+			else if (value == 0)
+				currentScene = 3;
+			else
+				currentScene = value;
+		}
+	}
+}
